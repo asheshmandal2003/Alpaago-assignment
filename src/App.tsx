d4d8e7ddf2d1a6a05dc987b71ea7d492763104cc
Ashesh Mandal <asheshmandal73@gmail.com>
2024-02-07 20:36:04 +0530

    added authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Signup from "./pages/Signup";
@@ -6,16 +6,33 @@ import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import ActiveUsers from "./pages/ActiveUsers";
 import Profile from "./pages/Profile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state: any) => state.auth.uid);
   return (
     <>
       <Routes>
-        <Route path="/auth/signup" element={<Signup />} />
-        <Route path="/auth/signin" element={<Signin />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<ActiveUsers />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/auth/signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/auth/signin"
+          element={!user ? <Signin /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/auth/signin" />}
+        />
+        <Route
+          path="/users"
+          element={user ? <ActiveUsers /> : <Navigate to="/auth/signin" />}
+        />
+        <Route
+          path="/profile"
+          element={user ? <Profile /> : <Navigate to="/auth/signin" />}
+        />
       </Routes>
       <ToastContainer />
     </>
